f9b41c234a9ec8f8ce6de0f401a73fd46a56eb5e
thulka <thulka@discretelogics.com>
2013-02-24 11:30:37 +0100

    inital - crousel

---
+++ js/countrylocator.js
@@ -0,0 +1,59 @@
+﻿/*	================================================================================
+$.Example.survey
+Author: 	 Philip Poole
+				
+Date:		 2010-01-30
+Description: Attemps to set the selected country in a drop down list of countries to
+the user's current location. 
+================================================================================ */
+
+(function ($) {
+  /**
+	* <p>Attemps to set the selected country in a drop down list of countries to
+	*    the user's current location.</p>
+	* 
+	* <p>For the script to work it is expect that each country is an option in a selected element
+	* and the value for each option is the offical country code for that country.</p>
+	*
+	* <p>For more information view http://welovenicethings.com/XXXXX
+	* 
+	* <p>The script uses an API call from http://www.wipmania.com to carry out the 
+	* Geolocation.</p>
+	*
+	* @param {string} cookieName The name of the cookie to set.
+	* 
+	* @example $.CountryLocator('ShippingAddress');
+	* @class
+	*/
+  $.fn.CountryLocator = function (cookieName) {
+
+    var $countrySelect = $(this);
+    var isClicked = false;
+
+    //add an event to the country select so if a user chooses a country before we do we do not overwrite that 
+    //choice, we also store the choice in a cookie so we do not attemp the API call next time. 
+    $countrySelect.click(function () {
+      $.cookie(cookieName, $countrySelect.val());
+      isClicked = true;
+    });
+
+
+    //if no cookie has been set (from a previous request) then call the API to find the users current country
+    if ($.cookie(cookieName) == null) {
+      $.getJSON('http://api.wipmania.com/jsonp?callback=?', '', function (json) {
+        //if the user has already selected a country then do not overwrite that choice.
+        if (!isClicked) {
+
+          //set the country to users current location
+          $countrySelect.children(' option[value=' + json.address.country_code + ']').attr('selected', 'selected');
+          $.cookie(cookieName, json.address.country_code);
+        }
+      });
+    } else {
+      var countryCode = $.cookie(cookieName);
+      $countrySelect.val(countryCode);
+    }
+    window.SetFlag();
+  };
+
+})(jQuery);
---
+++ js/shop.js
@@ -0,0 +1,191 @@
+﻿function ShowRequired($e, show)
+{
+  if (show)
+  {
+    $e.removeClass("shiftBack");
+  }
+  else
+  {
+    $e.addClass("shiftBack");
+  }
+}
+function IsEuropeanCountry(isoCountry)
+{
+  var countries = ["BE", "BE", "BG", "CZ", "DK", "DE", 
+                   "EE", "IE", "EL", "ES", "FR", "IT",
+                   "CY", "LV", "LT", "LU", "HU", "MT",
+                   "NL", "AT", "PL", "PT", "RO", "SI", 
+                   "SK", "FI", "SE", "GB"];
+  var found = $.inArray(isoCountry, countries);
+  return found != -1;
+}
+function UpdateFormDisplay($e)
+{
+  ShowRequired($e, $e.val() == "");
+}
+function SetFlag()
+{
+  $("#flag").attr("src", "../img/flags/" + $("#country").val().toLowerCase() + ".png");
+}
+function UpdateVAT()
+{
+  var c = $("#country").val();
+  if(c == "AT")
+  {
+    $("#vatidblock").hide();
+    window.taxrate = 0.2;
+  }
+  else
+  {
+    if(IsEuropeanCountry(c))
+    {
+      $("#vatidblock").show();
+      window.taxrate = 0.2;
+    }
+    else
+    {
+      $("#vatidblock").hide();
+      window.taxrate = 0;
+    }
+  }
+  Compute();
+}
+
+$.fn.checkval = function number() {
+  var v = $(this).val();
+  if (v * 1 == v && parseFloat(v) == parseInt(v) && v >= 0) {
+    return parseInt(v);
+  }
+  throw "format error";
+};
+
+function Compute()
+{
+  var p1 = $("#nTeaShell").checkval() * window.TeaShellPrice;
+  $("#teashelltotal").text(p1.toFixed(2));
+  var p2 = $("#nTeaHouse").checkval() * window.TeaHousePrice;
+  $("#teahousetotal").text(p2.toFixed(2));
+  var p3 = 0;
+  $("#teahousetrialtotal").text(p3.toFixed(2));
+
+  var p = p1 + p2;
+  var tax = p * window.taxrate;
+  if ($("#vatid").val() != "") tax = 0;
+  $("#tax").text(tax.toFixed(2));
+  $("#htax").val(tax.toFixed(2));
+  $("#total").text((p + tax).toFixed(2));
+}
+function getformvalues() {
+  var f, o, _i, _len, _ref, _results;
+  o = {};
+  _ref = $("form input, form select");
+  _results = [];
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    f = _ref[_i];
+    _results.push(o[f.name] = $(f).val());
+  }
+  return o;
+}
+$(document).ready(function ()
+{
+  $("#country").CountryLocator("Country");
+          
+  UpdateFormDisplay($("#firstName"));
+  UpdateFormDisplay($("#lastName"));
+  UpdateFormDisplay($("#company"));
+  UpdateFormDisplay($("#street"));
+  UpdateFormDisplay($("#city"));
+  UpdateFormDisplay($("#zip"));
+  UpdateFormDisplay($("#email"));
+
+  $("#personlic").focusin(function ()
+  {
+    $("#licenseTypeNote").show();
+  });
+  $("#personlic").focusout(function ()
+  {
+    $("#licenseTypeNote").hide();
+  });
+  $("#vatid").focusin(function ()
+  {
+    $("#vatidnote").show();
+  });
+  $("#vatid").focusout(function ()
+  {
+    $("#vatidnote").hide();
+  });
+  $("#corplic").click(function ()
+  {
+    $("#company").removeClass("hideBack");
+  });
+  $("#personlic").click(function ()
+  {
+    $("#company").addClass("hideBack");
+  });
+  $(".required input").keyup(function ()
+  {
+    var $e = $(this);
+    ShowRequired($e, $e.val() == "");
+  });
+  $("#country").keyup(function ()
+  {
+    SetFlag();
+    UpdateVAT();
+  });
+  $("#country").change(function ()
+  {
+    SetFlag();
+    UpdateVAT();
+  });
+
+  $("#nTeaShell, #nTeaHouse, #nTeaHouseTrial, #vatid")
+    .keyup(Compute)
+    .change(Compute);
+  
+  if (window.location.search == "?teahouse=trial") {
+    $("#nTeaShell").val(0);
+    $("#nTeaHouse").val(0);
+    $("#nTeaHouseTrial").val(1);
+  }
+
+  UpdateVAT();
+
+  $("form").submit(function(e)
+  {    
+    if  (
+        ($("#firstName").val() == "") ||
+        ($("#lastName").val() == "") ||
+        ($("#company").val() == "" && ($("#corplic").attr("checked") == true)) ||
+        ($("#street").val() == "") ||
+        ($("#city").val() == "") ||
+        ($("#zip").val() == "") ||
+        ($("#email").val() == "")
+        )
+    {
+      $("#requiredHint").show();
+      e.preventDefault();
+      return false;
+    };  
+    var cn = $("#country option:selected").text();
+    $("#CountryName").val(cn);            
+              
+    var order = getformvalues();
+    var json = JSON.stringify(order);
+    var cmd = {
+      url: "http://discretelogics.azurewebsites.net/shop/order",
+      // url: "http://localhost:5500/shop/order",
+      type: "POST",
+      contentType: "text", //
+      data: json,
+      success: function () {
+        window.location = "shopreceived";
+      },
+      error: function () {
+        alert("The confirmation email could not be sent, please contact us directly by email: office@discretelogics.com");
+      }
+    };
+    $.ajax(cmd);                
+    return false;
+  });
+});
+
